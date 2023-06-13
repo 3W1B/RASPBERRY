d9e9b725d4c39c694295774ee830ae98b37172b5
Marpond <sonkoly.marci02@gmail.com>
2023-06-13 09:37:35 +0200

    fixed issue with gps usb access, now has to be acm0

--- gps.py
+++ gps.py
@@ -4,13 +4,19 @@ import time
 
 
 def start():
-    serial_out = serial.Serial('/dev/ttyACM1', 9600, timeout=3)
+    serial_out = serial.Serial('/dev/ttyACM0', 9600, timeout=3)
     ubr = UBXReader(serial_out)
-    (raw_data, parsed_data) = ubr.read()
-    print(parsed_data)
     try :
-        latitude = parsed_data.lat
-        longitude = parsed_data.lon
-    except AttributeError:
+        (raw_data, parsed_data) = ubr.read()
+        print(parsed_data)
+        try:
+            latitude = parsed_data.lat
+            longitude = parsed_data.lon
+            if latitude is None or longitude is None:
+                return None
+        except AttributeError:
+            return None
+        return {'latitude': latitude, 'longitude': longitude}
+    except serial.serialutil.SerialException:
+        print("Error reading from gps.")
         return None
-    return {'latitude': latitude, 'longitude': longitude}
